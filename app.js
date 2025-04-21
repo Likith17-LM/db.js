1bd7fe457a4227345dd39098e9ba18f128ecebe5
Likith17-LM <nagalikithtalluri@gmail.com>
2025-04-20 21:40:49 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
+const express = require('express')
+const app = express()
+const api = require('./api')
+
+app.use(express.json())
+
+// Product routes
+app.post('/products', api.createProduct)
+app.get('/products', api.listProducts)
+app.get('/products/:id', api.getProduct)
+app.put('/products/:id', api.editProduct)
+app.delete('/products/:id', api.deleteProduct)
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`)
+})
 
