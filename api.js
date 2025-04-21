e9ff4c6ae1a15b6f90aafde882b79048a2ab1858
Likith17-LM <nagalikithtalluri@gmail.com>
2025-04-20 21:39:18 -0500

    Update api.js

--- api.js
+++ api.js
@@ -1,1 +1,44 @@
+const express = require('express')
+const Products = require('./products')
+const router = express.Router()
+
+// Create a product
+async function createProduct(req, res, next) {
+  const product = await Products.create(req.body)
+  res.json(product)
+}
+
+// List products
+async function listProducts(req, res, next) {
+  const { offset = 0, limit = 25, tag } = req.query
+  const products = await Products.list({ offset, limit, tag })
+  res.json(products)
+}
+
+// Get a product
+async function getProduct(req, res, next) {
+  const product = await Products.get(req.params.id)
+  res.json(product)
+}
+
+// Edit a product
+async function editProduct(req, res, next) {
+  const change = req.body
+  const product = await Products.edit(req.params.id, change)
+  res.json(product)
+}
+
+// Delete a product
+async function deleteProduct(req, res, next) {
+  const response = await Products.destroy(req.params.id)
+  res.json(response)
+}
+
+module.exports = {
+  createProduct,
+  listProducts,
+  getProduct,
+  editProduct,
+  deleteProduct
+}
 
