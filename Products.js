146a6a655783a7e1045f5cc6261edbd3f61a71e0
Likith17-LM <nagalikithtalluri@gmail.com>
2025-04-20 21:38:15 -0500

    Update Products.js

--- Products.js
+++ Products.js
@@ -1,1 +1,86 @@
+const cuid = require('cuid')
+const db = require('./db')
+
+// Define our Product Model
+const Product = db.model('Product', {
+  _id: { type: String, default: cuid },
+  description: { type: String },
+  alt_description: { type: String },
+  likes: { type: Number, required: true },
+  urls: {
+    regular: { type: String, required: true },
+    small: { type: String, required: true },
+    thumb: { type: String, required: true },
+  },
+  links: {
+    self: { type: String, required: true },
+    html: { type: String, required: true },
+  },
+  user: {
+    id: { type: String, required: true },
+    first_name: { type: String, required: true },
+    last_name: { type: String },
+    portfolio_url: { type: String },
+    username: { type: String, required: true },
+  },
+  tags: [{
+    title: { type: String, required: true },
+  }],
+})
+
+// Create a new product
+async function create(fields) {
+  const product = await new Product(fields).save()
+  return product
+}
+
+// List all products
+async function list(options = {}) {
+  const { offset = 0, limit = 25, tag } = options
+
+  const query = tag ? {
+    tags: {
+      $elemMatch: {
+        title: tag
+      }
+    }
+  } : {}
+
+  const products = await Product.find(query)
+    .sort({ _id: 1 })
+    .skip(offset)
+    .limit(limit)
+
+  return products
+}
+
+// Get a product
+async function get(_id) {
+  const product = await Product.findById(_id)
+  return product
+}
+
+// Edit a product
+async function edit(_id, change) {
+  const product = await get(_id)
+  Object.keys(change).forEach(key => {
+    product[key] = change[key]
+  })
+  await product.save()
+  return product
+}
+
+// Delete a product
+async function destroy(_id) {
+  return await Product.deleteOne({ _id })
+}
+
+// Export functions
+module.exports = {
+  create,
+  list,
+  get,
+  edit,
+  destroy
+}
 
